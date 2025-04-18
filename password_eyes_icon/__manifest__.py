a219c19e2c037d19acce4dd0d573c72e461c8ac2
Khai Dang <khaid@rikkeisoft.com>
2025-04-18 15:39:00 +0700

    password eyes icon 18.0

--- password_eyes_icon/__manifest__.py
+++ password_eyes_icon/__manifest__.py
@@ -18,7 +18,7 @@ Features:
     'license': 'LGPL-3',
     'depends': [
         'web', 
-        'auth_signup' 
+        # 'auth_signup' 
         # Uncomment this code if you want to show the icon on signup and reset password pages
     ],
     'data': [
